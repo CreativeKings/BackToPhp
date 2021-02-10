5a09ef9946c6ec5e73df41fa153003d8b35119f4
CreativeKings <70628543+CreativeKings@users.noreply.github.com>
2021-02-10 13:38:31 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php 
+    require("Models/Autoloader.php");
+    Autoloader::register();
+
+    if(!empty($_GET['id'])){
+        $jeu = new Jeu();
+        $jeu = $jeu->hydrate($_GET['id']);
+        if(empty($jeu)){
+            header("Location: index.php?erreur=LejeuNexistePas");
+            exit;
+        }
+        $vue = new Vues();
+
+        echo $vue->generateView($jeu, "jeu");
+    } else {
+        $jeux = Jeu::getAllJeux();
+        $vue = new Vues();
+
+        echo $vue->generateView($jeux);
+    }
+    
